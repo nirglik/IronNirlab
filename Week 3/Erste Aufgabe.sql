fd78fadfc9517a75178fa97666710990ce8e2647
Nir Glik <nirglik@gmail.com>
2022-11-01 09:51:58 +0100

    s

---
+++ Week 3/Erste Aufgabe.sql
@@ -0,0 +1,21 @@
+use sakila;
+SELECT * FROM actor;
+SELECT * FROM address;
+SELECT * FROM category;
+SELECT * FROM city;
+SELECT * FROM country;
+SELECT * FROM customer;
+SELECT * FROM film;
+SELECT * FROM film_actor;
+SELECT * FROM film_category;
+SELECT * FROM film_text;
+SELECT * FROM inventory;
+SELECT * FROM language;
+SELECT * FROM payment;
+SELECT * FROM rental;
+SELECT * FROM staff;
+SELECT * FROM store;
+SELECT title from film;
+SELECT count(store_id) FROM store;
+SELECT first_name from staff;
+
